6ca9af3d7002610c3dd62b6ff8ece286d1bfb1ef
Vivek Arote <vivek.arote@p99soft.com>
2026-04-08 22:01:13 +0530

    learned frontend basics and solved the assignments

---
+++ FrontendBootCamp/DAY01/app.js
@@ -0,0 +1,18 @@
+const users = {
+    1: {        
+        name: 'Alice',  
+        age: 30,    
+    },      
+    2: {    
+        name: 'Bob',        
+        age: 25,
+    },
+    3: {        
+        name: 'Charlie',    
+        age: 35,   
+        city: 'New York', 
+    },          
+}
+
+
+console.table(users)
